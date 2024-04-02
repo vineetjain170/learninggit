b748825edecf5b68f5830c71d4ca3b3cc2cd4b10
vineetjain170 <vineetjain170@gmail.com>
2024-04-03 00:12:48 +0530

    Test Commit

---
+++ TestSam.java
@@ -0,0 +1,12 @@
+class TestSam{
+    public static void main(String[] args){
+        SAMExample obj=(i)->{
+            for(int j=0;j<i;j++)System.out.println(j);
+            return i;
+        };
+        obj.loop(10);
+    }
+}
+public interface SAMExample{
+    public int loop(int i);
+}
